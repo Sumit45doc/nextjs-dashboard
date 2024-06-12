7d27bd89f35eacfef59ffd1047c550916517b5a4
Sumit Kumar <Sumit.SHARMA@3ds.com>
2024-06-12 22:17:09 +0530

    intial page as 1 for invoice table fixed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -19,7 +19,7 @@ export default async function Page({
 }) {
 
   const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 2
+  const currentPage = Number(searchParams?.page) || 1
   const totalPages = await fetchInvoicesPages(query)
 
   return (
